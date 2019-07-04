b0998102ffb2e3a6c672e8f74a5567cd2d693297
Avanade <Avanade@WIN-GW2C7ZXSME.corp.avanade.org>
2019-07-04 19:10:50 -0300

    testes auteração

--- Angular/Aula3-testes/src/app/soma.service.spec.ts
+++ Angular/Aula3-testes/src/app/soma.service.spec.ts
@@ -2,9 +2,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { SomaService } from './soma.service';
 
-describe('SomaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+describe('SomaService', () => {
+  let httpMock: HttpTestingController = null;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
+
+  beforeEach(() => {
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  
   it('should be created', () => {
     const service: SomaService = TestBed.get(SomaService);
     expect(service).toBeTruthy();
@@ -14,6 +25,26 @@ describe('SomaService', () => {
     const service: SomaService = TestBed.get(SomaService);
 
     expect(service.soma(2, 5)).toBeLessThan(8);
+  });
+
+  it('deve receber todos', () => {
+    const service = TestBed.get(SomaService);
+    const fakeData = [
+      { title: 'Title 1', description: 'Desc1' },
+      { title: 'Title 2', description: 'Desc2' },
+    ];
+
+    service.getTodos().subscribe(v => {
+      console.log(v);
+      expect(v.length).toBe(2);
+      expect(v[0].date).toBeUndefined();
+      expect(v[1].title).toBe('Title 2');
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000');
+    expect(req.request.method).toBe('GET');
+    req.flush(fakeData);
+
   });
 
 
--- Angular/Aula3-testes/src/app/soma.service.ts
+++ Angular/Aula3-testes/src/app/soma.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SomaService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   soma(n1, n2){
     return n1 + n2;
   }
 
+
+  getTodos(){
+    return this.httpClient.get('http://localhost:3000');  
+
+  }
 
 }
